854eb985b68738cd824fc3017b0084bf8d0d6cd5
pratik shelar <pratikshelar1727@gmail.com>
2024-07-22 19:16:57 +0530

    submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         B b = new B();
         System.out.println(b.meth());
+        System.out.println("hello world2");
     }
 
     public static class A {
