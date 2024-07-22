88ded44a54b49c57301563340189e48fd0b2bda6
pratik shelar <pratikshelar1727@gmail.com>
2024-07-22 11:47:21 +0530

    adding main method in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B b = new B();
+        String ans = b.meth();
+        System.out.println(ans);
+    }
+}
+class A{
+    public String meth(){
+        return "Hi. from class A";
+    }
+}
+class B extends A{
+
 }
