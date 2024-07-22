d1e0d5fa69e707c57fb6bf10dc77a666734a2f3d
pratik shelar <pratikshelar1727@gmail.com>
2024-07-22 17:53:15 +0530

    assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,6 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B b = new B();
-        String ans = b.meth();
-    }
-}
-class A{
-    public String meth(){
-        return  "Invoking method from class A";
-    }
-}
-class B extends A {
-    public String meth() {
-        return  "Method is overridden in Extended class B";
+        System.out.println(b.meth());
     }
 }
